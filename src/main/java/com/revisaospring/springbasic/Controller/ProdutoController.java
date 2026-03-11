9f7baee5d14490c4f01c679bf74f2d6ba6c57405
4l3xandr3bs <bobertbohemianbobux@gmail.com>
2026-03-11 14:08:45 -0300

    rEposting zebra

--- src/main/java/com/revisaospring/springbasic/Controller/ProdutoController.java
+++ src/main/java/com/revisaospring/springbasic/Controller/ProdutoController.java
@@ -1,5 +1,25 @@
 package com.revisaospring.springbasic.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.revisaospring.springbasic.Service.ProdutoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/produtoCTR")
 public class ProdutoController {
+    @Autowired
+    private ProdutoService oProdutoService;
+    @GetMapping("/listarprodutos")
+    public String telaListarProduto(Model oModel) {
+        oModel.addAttribute("produtos", oProdutoService.buscarTodos());
+        return "listarProdutos";
+    }
     
+
 }
